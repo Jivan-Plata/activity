e993e9495a21c49574fada450fa7313456f0edf5
Jivan-Plata <jivanplata@gmail.com>
2023-03-20 16:19:14 +0800

    Add activity code

---
+++ indexActivity.js
@@ -0,0 +1,23 @@
+console.log("Hello, Welcome to De La Salle University")
+
+let firstName = "Jivan"
+let lastName = "Plata"
+let age = 1
+let hobbies = ['Playing the guitar', 'Volleyball', 'Calisthenics', 'Watching YouTube videos', 'Playing video games'];
+let address = {
+	houseNumber: 132,
+	street: 'Green Meadows Avenue',
+	subdivision: 'Green Meadows at the Orchard',
+	barangay: 'Salawag',
+	city: 'Dasmarinas',
+	province: 'Cavite',
+	zipCode: 4114
+}
+
+console.log(firstName);
+console.log(lastName);
+console.log(age);
+console.log(hobbies);
+console.log(address);
+
+
